19ac79adec4e2e49d12d8af7cef402c9de74334e
uyoung <i@uyoung.co>
2019-04-04 16:34:46 +0800

    提交

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,6 @@
 const book = {
-  test: state => state.book.test
+  fileName: state => state.book.fileName,
+  menuVisible: state => state.book.menuVisible
 }
 
 export default book
--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -1,16 +1,14 @@
 const book = {
   state: {
-    test: 1
+    fileName: '',
+    menuVisible: false
   },
   mutations: {
-    'SET_TEST': (state, newTest) => {
-      state.test = newTest
-    }
-  },
-  actions: {
-    setTest: ({ commit, state }, newTest) => {
-
-      commit('SET_TEST', newTest)
+    'SET_FILENAME': (state, fileName) => {
+      state.fileName = fileName
+    },
+    'SET_MENUVISIBLE': (state, menuVisible) => {
+      state.menuVisible = menuVisible
     }
   }
 }
