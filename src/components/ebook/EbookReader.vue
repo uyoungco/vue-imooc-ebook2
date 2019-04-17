38fc8ababb7c76d6bdfa5ce231c251527340f222
uyoung <i@uyoung.co>
2019-04-17 13:09:46 +0800

    完善页脚分页显示

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -6,6 +6,9 @@
       @click="onMaskClick"
       @touchmove="move"
       @touchend="moveEnd"
+      @mousedown.left="onMouseEnter"
+      @mousemove.left="onMouseMove"
+      @mouseup.left="onMouseEnd"
     ></div>
   </div>
 </template>
@@ -34,6 +37,46 @@
       })
     },
     methods: {
+      // 1 - 鼠标进入
+      // 2 - 鼠标进入后的移动
+      // 3 - 鼠标从移动状态松手
+      // 4 - 鼠标还原
+      onMouseEnd(e) {
+        if (this.mouseState === 2) {
+          this.setOffsetY(0)
+          this.firstOffsetY = null
+          this.mouseState = 3
+        } else {
+          this.mouseState = 4
+        }
+        const time = e.timeStamp - this.mouseStartTime
+        if (time < 100) {
+          this.mouseState = 4
+        }
+        e.preventDefault()
+        e.stopPropagation()
+      },
+      onMouseMove(e) {
+        if (this.mouseState === 1) {
+          this.mouseState = 2
+        } else if (this.mouseState === 2) {
+          let offsetY = 0
+          if (this.firstOffsetY) {
+            offsetY = e.clientY - this.firstOffsetY
+            this.setOffsetY(offsetY)
+          } else {
+            this.firstOffsetY = e.clientY
+          }
+        }
+        e.preventDefault()
+        e.stopPropagation()
+      },
+      onMouseEnter(e) {
+        this.mouseState = 1
+        this.mouseStartTime = e.timeStamp
+        e.preventDefault()
+        e.stopPropagation()
+      },
       move(e) {
         let offsetY = 0
         if (this.firstOffsetY) {
@@ -50,6 +93,9 @@
         this.firstOffsetY = null
       },
       onMaskClick(e) {
+        if (this.mouseState && (this.mouseState === 2 || this.mouseState === 3)) {
+          return
+        }
         const offsetX = e.offsetX
         const width = window.innerWidth
         if (offsetX > 0 && offsetX < width * 0.3) {
@@ -121,6 +167,7 @@
           width: innerWidth,
           height: innerHeight,
           method: 'default'
+          // flow: 'scrolled' // 上下滚动翻页模式  在ios默认浏览器和微信支持不是很好
         })
         const location = getLocation(this.fileName)
         this.display(location, () => {
@@ -186,6 +233,30 @@
         this.book.ready.then(() => {
           return this.book.locations.generate(750 * (window.innerWidth / 375) * (getFontSize(this.fileName) / 16))
         }).then(locations => {
+          this.navigation.forEach(nav => {
+            nav.pagelist = []
+          })
+          locations.forEach(item => {
+            const loc = item.match(/\[(.*)\]!/)[1]
+            this.navigation.forEach(nav => {
+              if (nav.href) {
+                const href = nav.href.match(/^(.*)\.html$/)[1]
+                if (href === loc) {
+                  nav.pagelist.push(item)
+                }
+              }
+            })
+            let currentPage = 1
+            this.navigation.forEach((nav, index) => {
+              if (index === 0) {
+                nav.page = 1
+              } else {
+                nav.page = currentPage
+              }
+              currentPage += nav.pagelist.length + 1
+            })
+          })
+          this.setPagelist(locations)
           this.setBookAvailable(true)
           this.refreshLocation()
         })
--- src/components/ebook/EbookSlideContents.vue
+++ src/components/ebook/EbookSlideContents.vue
@@ -25,8 +25,12 @@
           <img :src="cover" alt="" class="slide-contents-book-img">
         </div>
         <div class="slide-contents-book-info-wrapper">
-          <div class="slide-contents-book-title">{{metadata.title}}</div>
-          <div class="slide-contents-book-author">{{metadata.creator}}</div>
+          <div class="slide-contents-book-title">
+            <span class="slide-contents-book-title-text">{{metadata.title}}</span>
+          </div>
+          <div class="slide-contents-book-author">
+            <span class="slide-contents-book-author-text">{{metadata.creator}}</span>
+          </div>
         </div>
         <div class="slide-contents-book-progress-wrapper">
           <div class="slide-contents-book-progress">
@@ -49,7 +53,7 @@
             :class="{'selected': section === index}"
             @click="displayContent(item.href)"
           >{{item.label}}</span>
-          <span class="slide-contents-item-page"></span>
+          <span class="slide-contents-item-page">{{item.page}}</span>
         </div>
       </scroll>
       <scroll
@@ -187,16 +191,23 @@
         padding: 0 px2rem(10);
         box-sizing: border-box;
         .slide-contents-book-title {
-          width: px2rem(153.75);
+          /*width: px2rem(153.75);*/
           font-size: px2rem(14);
           line-height: px2rem(16);
-          @include ellipsis2(2)
+          @include left;
+          .slide-contents-book-title-text {
+            @include ellipsis2(2)
+          }
         }
         .slide-contents-book-author {
-          width: px2rem(153.75);
+          /*width: px2rem(153.75);*/
           font-size: px2rem(12);
+          line-height: px2rem(14);
           margin-top: px2rem(5);
-          @include ellipsis;
+          @include left;
+          .slide-contents-book-author-text {
+            @include ellipsis2(1);
+          }
         }
       }
       .slide-contents-book-progress-wrapper {
@@ -233,7 +244,8 @@
           @include ellipsis;
         }
         .slide-contents-item-page {
-
+          flex: 0 0 px2rem(30);
+          font-size: px2rem(10);
         }
       }
     }
